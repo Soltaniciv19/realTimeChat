ea8dcfca4dd205a25e13ee3601bf38eaccba5932
VasileS <VasileS@gilat.com>
2024-06-04 10:33:33 +0300

    Added method sendMessageToServer which is taking an socket and is sending a message.

--- src/main/java/chat/entities/Client.java
+++ src/main/java/chat/entities/Client.java
@@ -1,16 +1,21 @@
 package chat.entities;
 
+import chat.Models.Message;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Client {
     public Socket connectToServer(String serverAddress, int port) throws IOException{
         System.out.println("Connecting to server: " + serverAddress + ":" + port);
 
-        try (Socket socket = new Socket(serverAddress,port)){
+        try {
+            Socket socket = new Socket(serverAddress,port);
             System.out.println("Successfully connected to server!");
             return socket;
 
@@ -21,8 +26,25 @@ public class Client {
         return null;
     }
 
-    public static void main(String[] args) throws IOException {
-        Client client = new Client();
-        client.connectToServer("localhost",8081);
+    public void sendMessageToServer(Socket socket,String content) throws IOException{
+        Random random = new Random();
+        Message message = new Message(random.nextInt(1001),content);
+
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+        objectOutputStream.writeObject(message);
+
     }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        Client client = new Client();
+        Socket clientServerConnection = client.connectToServer("localhost",8081);
+
+        while (true){
+            System.out.println("Enter you message");
+            String messageContent = scanner.nextLine();
+            client.sendMessageToServer(clientServerConnection, messageContent);
+        }
+    }
+
 }
--- src/main/java/chat/entities/Server.java
+++ src/main/java/chat/entities/Server.java
@@ -1,5 +1,34 @@
 package chat.entities;
 
-public class Server {
+import chat.Models.Message;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void runServer(int port) throws IOException {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server running on port: " + port);
+
+            while (true){
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected");
+                ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
+
+                Message clientRequest = (Message) objectInputStream.readObject();
+                System.out.println(clientRequest.getBody());
+            }
+
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        runServer(8081);
+    }
 }
